7c8ae1e40385564cdee9ca8c272653965095b72f
alexandre <beaudouina@gmail.com>
2017-09-27 15:04:43 +0200

    mise en place projet

---
+++ src/components/home.js
@@ -0,0 +1,21 @@
+import React, {Component} from 'react';
+
+
+class Home extends Component{
+
+	render(){
+		return(
+			<div className="site-wrapper">
+				<div className="site-wrapper-inner">
+					<div className="cover-container">
+							Bonjour, Alexandre<br/>
+							Développeur Front-end<span className="point">.</span>
+					</div>
+				</div>
+			</div>
+			)
+	}
+}
+
+
+export default Home
